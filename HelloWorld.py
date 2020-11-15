951105434b90822ed84d2c012d8844ab211f4be7
Nikiforova <alyona.nikiforova@intel.com>
2020-11-15 21:26:43 +0300

    = third commit =

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 print("Hello World.")
 
 x = 3
@@ -40,4 +43,36 @@ third_disc = {1: ["Horse", "Dog"], 2: ["Horse", "Cat"], 3: ["Mule"]}
 
 print(third_disc[2])
 
+for index in  range(2,10,2):
+    print (index)
+# Handle exceptions with a try/except block
 
+# Handle exceptions with a try/except block
+try:
+    # Use "raise" to raise an error
+    raise IndexError("This is an index error")
+except IndexError as e:
+    pass                 # Pass is just a no-op. Usually you would do recovery here.
+except (TypeError, NameError):
+    pass                 # Multiple exceptions can be handled together, if required.
+else:                    # Optional clause to the try/except block. Must follow all except blocks
+    print("All good!")   # Runs only if the code in try raises no exceptions
+finally:                 #  Execute under all circumstances
+    print("We can clean up resources here")
+
+try:
+    raise EOFError()
+    raise IndexError("45345435")
+except:
+    pass
+
+
+# contents = {"aa": 12, "bb": 21}
+# with open("myfile1.txt", "w+") as file:
+#     file.write(str(contents))        # writes a string to a file
+
+with open("myfile1.txt") as file:
+    content = json.load(file)
+    print(content['aa'])
+
+print(os.getcwd())    
